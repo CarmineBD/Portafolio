2801e61be64ccd4441cd88ab59ef2e8c6d9441d0
Carmine Buonaiuto <carmine-bnt@hotmail.com>
2022-11-24 10:49:29 +0100

    primera subida

---
+++ main.js
@@ -0,0 +1,27 @@
+function preview() {
+    document.body.innerHTML += `    
+        
+    <div class="container-fluid blur-background">
+        
+        <section class="container preview-window">
+            <div class="row">
+                <div class="col-6">
+                    <h2>Titulo del proyecto</h2>
+                    <h3>Descripción<h3>
+                    <p>Este fue mi primer proyecto web, y fue proyecto final de un bootcamp de front-end, está hecho con lo básico y con todo lo que he aprendido en aqul bootcamp, objetivo de este proyecto era hacer el front de una página web para adoptar perros</p>
+
+                    <h3>Tecnologías utilizadas<h3>
+                    <p>Este fue mi primer proyecto web, y fue proyecto final de un bootcamp de front-end, está hecho con lo básico y con todo lo que he aprendido en aqul bootcamp, objetivo de este proyecto era hacer el front de una página web para adoptar perros</p>
+                </div>
+                <div class="row col-6">
+                    <div class="col-10 bg-primary">a</div>
+                    <div class="col-2 bg-warning">a</div>
+                </div>
+            </div>
+            <div class="row bg-warning">footer</div>
+        </section>
+    </div>
+    
+    
+    `
+}
